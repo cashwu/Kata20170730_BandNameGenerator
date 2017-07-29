29f4c37a1af1907050b55c8511a008b5fb7f92ce
cash <cash@cashwu.com>
2017-07-29 09:10:37 +0800

    knife test case

--- Kata20170730_BandNameGenerator/BandNameGeneratorTests.cs
+++ Kata20170730_BandNameGenerator/BandNameGeneratorTests.cs
@@ -7,8 +7,19 @@ namespace Kata20170730_BandNameGenerator
     public class BandNameGeneratorTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void knife_should_return_The_knife()
         {
+            var kata = new Kata();
+            var acutal = kata.BandNameGenerator("knife");
+            Assert.AreEqual("The knife", acutal);
+        }
+    }
+
+    public class Kata
+    {
+        public string BandNameGenerator(string str)
+        {
+            return "The " + str;
         }
     }
 }
