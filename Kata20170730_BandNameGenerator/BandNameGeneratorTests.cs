361ba2b791e7f49856f4ddb12a87a13e737f9b62
cash <cash@cashwu.com>
2017-07-29 09:00:43 +0800

    init

--- Kata20170730_BandNameGenerator/BandNameGeneratorTests.cs
+++ Kata20170730_BandNameGenerator/BandNameGeneratorTests.cs
@@ -4,7 +4,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace Kata20170730_BandNameGenerator
 {
     [TestClass]
-    public class UnitTest1
+    public class BandNameGeneratorTests
     {
         [TestMethod]
         public void TestMethod1()
